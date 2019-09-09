2d84e93d445b5296819e3c9b7d3f28a48a7f67fe
prasobhk <prasobh@obtil.com>
2019-09-09 09:19:46 +0200

    code optimized ....

--- src/main/java/com/company/plangenerator/services/PlanGeneratorService.java
+++ src/main/java/com/company/plangenerator/services/PlanGeneratorService.java
@@ -41,23 +41,22 @@ public class PlanGeneratorService {
 
 	private RepaymentPlans generateRepaymentPlans(LoanDetails loanDetails) {
 
-		final double nominalRate = loanDetails.getNominalRate();
-		double loanAmount = loanDetails.getLoanAmount();
-		final int loanDuration = loanDetails.getDuration();
-		final double monthlyRate = (nominalRate / Constants.NO_OF_MONTHS_IN_A_YEAR) / Constants.HUNDRED;
 		final RepaymentPlans repaymentPlans = new RepaymentPlans();
 		final List<RepaymentPlan> repaymentPlansList = new ArrayList<>();
-		OffsetDateTime paymentDate = loanDetails.getStartDate();
 		repaymentPlans.setRepaymentPlans(repaymentPlansList);
-		generateRepaymentPlanForPendingDurations(loanDetails, nominalRate, loanAmount, loanDuration, monthlyRate,
-				repaymentPlansList, paymentDate);
+		generateRepaymentPlanForPendingDurations(loanDetails, repaymentPlansList);
 
 		return repaymentPlans;
 	}
 
-	private void generateRepaymentPlanForPendingDurations(LoanDetails loanDetails, final double nominalRate,
-			double loanAmount, final int loanDuration, final double monthlyRate,
-			final List<RepaymentPlan> repaymentPlansList, OffsetDateTime paymentDate) {
+	private void generateRepaymentPlanForPendingDurations(LoanDetails loanDetails,
+			final List<RepaymentPlan> repaymentPlansList) {
+		final double nominalRate = loanDetails.getNominalRate();
+		final double monthlyRate = (nominalRate / Constants.NO_OF_MONTHS_IN_A_YEAR) / Constants.HUNDRED;
+		final int loanDuration = loanDetails.getDuration();
+		double loanAmount = loanDetails.getLoanAmount();
+		OffsetDateTime paymentDate = loanDetails.getStartDate();
+		// finding re-payment for each month
 		for (int pendingPaymentInMonth = loanDuration; pendingPaymentInMonth > Constants.ZERO; pendingPaymentInMonth--) {
 			final RepaymentPlan repaymentPlan = new RepaymentPlan();
 			final double interest = ((nominalRate / Constants.HUNDRED * Constants.NO_OF_DAYS_IN_A_MONTH * loanAmount)
@@ -66,13 +65,6 @@ public class PlanGeneratorService {
 			repaymentPlan.setInterest(roundVal(interest));
 			repaymentPlan.setInitialOutstandingPrincipal(roundVal(loanAmount));
 			repaymentPlan.setDate(paymentDate);
-			final double futureValue = calculateFutureValueOfTheLoanAmount(loanAmount, nominalRate / Constants.HUNDRED,
-					loanDuration);
-			final double presentValue = calculatePresentValueOfLoanAmount(futureValue, nominalRate / Constants.HUNDRED,
-					loanDuration);
-
-			logger.debug("presentValue  " + presentValue);
-
 			final double borrowerPaymentAmount = roundVal((loanAmount * monthlyRate)
 					/ (Constants.ONE - Math.pow(Constants.ONE + monthlyRate, -pendingPaymentInMonth)));
 			repaymentPlan.setBorrowerPaymentAmount(borrowerPaymentAmount);
@@ -97,15 +89,5 @@ public class PlanGeneratorService {
 
 	private double roundVal(double val) {
 		return new BigDecimal(val).setScale(Constants.TWO, RoundingMode.HALF_UP).doubleValue();
-	}
-
-	private double calculateFutureValueOfTheLoanAmount(double investmentAmount, double annualInterestRate,
-			double months) {
-		return investmentAmount
-				* Math.pow(Constants.ONE + (annualInterestRate / Constants.NO_OF_MONTHS_IN_A_YEAR), months);
-	}
-
-	public double calculatePresentValueOfLoanAmount(double futureValue, double annualInterestRate, int months) {
-		return futureValue / Math.pow(Constants.ONE + annualInterestRate, months / Constants.NO_OF_MONTHS_IN_A_YEAR);
 	}
 }
